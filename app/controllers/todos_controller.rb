64e609e22fda44950df09574d8f0c85eb552018f
Carlota Arzua Alonso <carlotaarzua@MacBook-Pro.local>
2024-09-17 11:48:22 -0500

    Implemented Edit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:edit, :update]
+
   def new
     @todo = ToDo.new
   end
@@ -13,7 +15,23 @@ class TodosController < ApplicationController
     end
   end
 
+  def edit
+    # @todo bject set by the before_action
+  end
+
+  def update
+    if @todo.update(todo_params)
+      redirect_to todos_path, notice: "ToDo item was successfully updated"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def set_todo
+    @todo = ToDo.find(params[:id])
+  end
 
   def todo_params
     params.require(:to_do).permit(:title, :description, :due_date)

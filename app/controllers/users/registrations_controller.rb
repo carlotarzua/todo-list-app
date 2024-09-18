b6fec15e19100315ac4d508f29cb0988637ecdeb
bkoz8 <briankoz100@gmail.com>
2024-09-18 15:45:15 -0500

    User management + style edited

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,16 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+    before_action :configure_account_update_params, only: [:update]
+
+    # Configure additional parameters for account updates
+    def configure_account_update_params
+        devise_parameter_sanitizer.permit(:account_update, keys: [:email, :email, :email, :password, :password_confirmation, :current_password])
+    end
+
+    # Redirect to the sign-in page after updating the profile
+    def after_update_path_for(resource)
+      sign_out(resource)
+      new_user_session_path
+    end
+
+  end
+  

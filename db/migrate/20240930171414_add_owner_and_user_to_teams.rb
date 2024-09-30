db67c32e071e7a9e849912d9d35602000588fc16
Carlota Arzua Alonso <carlotaarzua@MacBook-Pro.local>
2024-09-30 12:51:36 -0500

    Fixed duplicated owner_id column

---
+++ db/migrate/20240930171414_add_owner_and_user_to_teams.rb
@@ -0,0 +1,6 @@
+class AddOwnerAndUserToTeams < ActiveRecord::Migration[7.2]
+  def change
+    #add_reference :teams, :owner, foreign_key: { to_table: :users }
+    add_reference :teams, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_30_033530) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_30_171414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,12 +21,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_30_033530) do
   end
 
   create_table "teams", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.bigint "owner_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "owner_id"
+    t.bigint "user_id", null: false
     t.index ["owner_id"], name: "index_teams_on_owner_id"
     t.index ["user_id"], name: "index_teams_on_user_id"
   end
@@ -45,8 +45,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_30_033530) do
     t.datetime "updated_at", null: false
     t.string "priority"
     t.boolean "completed"
-    t.string "reminder"
     t.bigint "category_id"
+    t.string "reminder"
     t.string "email"
     t.datetime "due_datetime"
     t.datetime "start_time"

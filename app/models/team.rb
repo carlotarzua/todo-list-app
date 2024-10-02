0222a916566dd371e2374166852fa5ce5b0dc706
Carlota Arzua Alonso <carlotaarzua@MacBook-Pro.local>
2024-10-02 13:04:30 -0500

    Shared ToDo List and Create Shared ToDo Items

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,7 @@
 class Team < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
   has_and_belongs_to_many :users
+  has_many :to_dos, dependent: :destroy
   validates :name, presence: true
   validates :owner_id, presence: true
 end
--- db/migrate/20241002041259_add_team_id_to_todos.rb
+++ db/migrate/20241002041259_add_team_id_to_todos.rb
@@ -1,5 +1,5 @@
 class AddTeamIdToTodos < ActiveRecord::Migration[7.2]
   def change
-    add_column :to_dos, :team_id, :integer
+    #add_column :to_dos, :team_id, :integer
   end
 end
